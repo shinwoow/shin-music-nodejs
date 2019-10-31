a30c758c04936e0c5e869b08d28ac0375d0217ca
shin <1109323254@qq.com>
2019-10-31 22:09:38 +0800

    添加音乐 静态文件目录
    添加音乐 静态文件目录

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(bodyparser({
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
+app.use(require('koa-static')(__dirname + '/uploads'))
+
 
 app.use(views(__dirname + '/views', {
   extension: 'pug'
