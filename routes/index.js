f363d7fb986b9dcd6a2a358a3c952d152889cbec
shin <1109323254@qq.com>
2019-10-31 20:00:25 +0800

    新增文件上传api
    新增文件上传api（待迭代）

---
+++ routes/index.js
@@ -0,0 +1,71 @@
+const mysql = require('../mysql')
+const Router = require('koa-router')
+const fs = require('fs')
+const path = require('path')
+
+//实例化路由， 拼接到api路径下，restful规范
+const router = new Router({
+  prefix: '/api'
+});
+
+router.get('/hellow/:name', async (ctx, next) => {
+  var name = ctx.params.name;
+  ctx.response.body = `<h1>Hello, ${name}!</h1>`
+});
+
+
+//获取用户信息
+router.get('/user', async (ctx, next) => {
+  let data = await mysql.getUser()
+  ctx.add
+  ctx.body = {
+    "code": 1,
+    "data": data,
+    "mesg": 'ok'
+  }
+  next()
+})
+
+//获取用户信息
+router.get('/music', async (ctx, next) => {
+  let data = await mysql.getMusicList()
+  ctx.add
+  ctx.body = {
+    "code": 1,
+    "data": data,
+    "mesg": 'ok'
+  }
+  next()
+})
+
+
+// 定义文件上传路由
+router.post('/uploadFilds', async (ctx, next) => {
+
+  const files = ctx.request.files.file;
+  let count = 0;
+  files.forEach(file => {
+    // 创建可读流
+    const reader = fs.createReadStream(file.path);
+
+    let targetPath = path.join(__dirname, '../uploads/') + `/${file.name}`;
+    //创建可写流
+    const upStream = fs.createWriteStream(targetPath);
+    // 可读流通过管道写入可写流
+    reader.pipe(upStream);
+    count++;
+
+  });
+  //返回上传结果
+  return ctx.body = {
+    code: 200,
+    data: {
+      msg: `共上传文件:${
+        files.length
+      }个，上传成功${count}个`
+    }
+  };
+})
+
+//导出router
+module.exports = router
